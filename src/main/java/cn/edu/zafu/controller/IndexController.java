347a0a66ff641fd39fd7cbb1f04ffe5e7621513d
李樟取 <lizhangqu@weidian.com>
2016-01-24 11:36:21 +0800

    日志配置

--- src/main/java/cn/edu/zafu/controller/IndexController.java
+++ src/main/java/cn/edu/zafu/controller/IndexController.java
@@ -1,5 +1,7 @@
 package cn.edu.zafu.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,11 +10,21 @@ import org.springframework.web.servlet.ModelAndView;
  * Created by lizhangqu on 16/1/24.
  */
 @Controller
+@RequestMapping("index")
 public class IndexController {
 
-    @RequestMapping("/index")
+    @RequestMapping("index")
     public ModelAndView index(){
+        Logger logger = LoggerFactory.getLogger("lizhangqu");
+        logger.error("index error");
         return new ModelAndView("index/index");
+    }
+
+    @RequestMapping("test")
+    public String test(){
+        Logger logger = LoggerFactory.getLogger("lizhangqu");
+        logger.error("test error");
+        return "index/test";
     }
 
 }
