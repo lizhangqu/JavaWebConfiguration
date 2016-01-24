00295c7a61aab39f8d2d91c9e97736a118efcf10
李樟取 <lizhangqu@weidian.com>
2016-01-24 14:28:38 +0800

    支持文件上传

---
+++ src/main/java/cn/edu/zafu/controller/UploadController.java
@@ -0,0 +1,46 @@
+package cn.edu.zafu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+
+/**
+ * Created by lizhangqu on 16/1/24.
+ */
+@Controller
+@RequestMapping("upload")
+public class UploadController {
+
+    @RequestMapping("index")
+    public String index(){
+        return "upload/index";
+    }
+    @RequestMapping(value = "upload")
+    public String upload(@RequestParam(value = "file", required = false) MultipartFile file, HttpServletRequest request, ModelMap model) {
+
+        System.out.println("开始");
+        String path = request.getSession().getServletContext().getRealPath("upload");
+        String fileName = file.getOriginalFilename();
+//        String fileName = new Date().getTime()+".jpg";
+        System.out.println(path);
+        File targetFile = new File(path, fileName);
+        if(!targetFile.exists()){
+            targetFile.mkdirs();
+        }
+        //保存
+        try {
+            file.transferTo(targetFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        model.addAttribute("fileUrl", request.getContextPath()+"/upload/"+fileName);
+
+        return "upload/result";
+    }
+
+}
