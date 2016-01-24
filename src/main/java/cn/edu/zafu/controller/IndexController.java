f357c15ce24189a4e7e745080a8de1eda261d34d
李樟取 <lizhangqu@weidian.com>
2016-01-24 11:03:28 +0800

    volocity 模板支持

---
+++ src/main/java/cn/edu/zafu/controller/IndexController.java
@@ -0,0 +1,18 @@
+package cn.edu.zafu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by lizhangqu on 16/1/24.
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping("/index")
+    public ModelAndView index(){
+        return new ModelAndView("index/index");
+    }
+
+}
